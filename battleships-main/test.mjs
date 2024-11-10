f9927d5f35b9acfdac3968f7a740b22416ed0afe
ianedmar <iecastillo@uia.no>
2024-11-10 21:04:22 +0100

    Delete battleships-main directory

--- battleships-main/test.mjs
+++
@@ -1,9 +0,0 @@
-const GAME_BOARD_DIM = 10;
-
-let borderTop = "+".padEnd(GAME_BOARD_DIM + 2, "-") + "+";
-let rowDisplay = "|".padEnd(GAME_BOARD_DIM + 2, "・") + "|";
-
-console.log(borderTop);
-console.log(rowDisplay);
-
-console.log(borderTop.length, rowDisplay.length);
